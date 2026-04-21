4afdeb4527a05db8cf420e38956d2c1c12a7f812
GardenSoup23755 <bobsmells129@gmail.com>
2026-04-21 04:19:30 +0100

    Update ClipRecordingService.cs

--- BraveClipping/Services/ClipRecordingService.cs
+++ BraveClipping/Services/ClipRecordingService.cs
@@ -1,14 +1,29 @@
 using ScreenRecorderLib;
+using System;
 
 public class ClipRecordingService
 {
-    private Recorder _recorder;
+    private Recorder? _recorder;
+
+    public event Action<string>? RecordingCompleted;
+    public event Action<string>? RecordingFailed;
 
     public void StartRecording(string filePath)
     {
         var options = new RecorderOptions();
 
         _recorder = Recorder.CreateRecorder(options);
+
+        _recorder.OnRecordingComplete += (s, e) =>
+        {
+            RecordingCompleted?.Invoke(filePath);
+        };
+
+        _recorder.OnRecordingFailed += (s, e) =>
+        {
+            RecordingFailed?.Invoke(e.Error);
+        };
+
         _recorder.Record(filePath);
     }
 
